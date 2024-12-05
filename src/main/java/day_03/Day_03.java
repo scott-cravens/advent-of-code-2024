4e0c90dcd555839be7bdc1da308f52084374663a
scott-cravens <scravens@gmail.com>
2024-12-05 11:14:56 -0500

    Day 03 Part 1 and 2, initial commit

---
+++ src/main/java/day_03/Day_03.java
@@ -0,0 +1,46 @@
+package day_03;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static java.lang.String.format;
+
+public class Day_03 {
+
+    public static void main(String[] args) {
+        List<String> input = readInputData();
+        final Pattern pattern = Pattern.compile("mul\\((\\d+),(\\d+)\\)|(do|don't)\\(\\)");
+
+        boolean inDoBlock = true;
+        int resultPart1 = 0, resultPart2 = 0;
+        for (String s : input) {
+            Matcher matcher = pattern.matcher(s);
+            while (matcher.find()) {
+                if (matcher.group(0).startsWith("mul")) {
+                    int product = Integer.parseInt(matcher.group(1)) * Integer.parseInt(matcher.group(2));
+                    resultPart1 += product;
+                    if (inDoBlock) resultPart2 += product;
+                } else {
+                    inDoBlock = !matcher.group(0).startsWith("don't");
+                }
+            }
+        }
+
+        System.out.printf("Part 1: %d\n", resultPart1);
+        System.out.printf("Part 2: %d\n", resultPart2);
+    }
+
+    private static List<String> readInputData() {
+        String resourceName = "Day_03_input.txt";
+        try {
+            Path path = Paths.get(ClassLoader.getSystemResource(resourceName).toURI());
+            return Files.readAllLines(path);
+        } catch (Exception e) {
+            throw new RuntimeException(format("Error reading the file: %s%n", e.getMessage()));
+        }
+    }
+}
